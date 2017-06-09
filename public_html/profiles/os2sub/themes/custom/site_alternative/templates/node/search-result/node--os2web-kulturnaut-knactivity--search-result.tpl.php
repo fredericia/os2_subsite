13ab76d2d4915a5136c14183f5a54a8e6e1d08f1
dsbellcom <ds@bellcom.ee>
2017-06-09 09:49:37 +0300

    kommende_arangementer design (desktop + mobile)

--- public_html/profiles/os2sub/themes/custom/site_alternative/templates/node/search-result/node--os2web-kulturnaut-knactivity--search-result.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_alternative/templates/node/search-result/node--os2web-kulturnaut-knactivity--search-result.tpl.php
@@ -81,10 +81,10 @@
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="row">
-    <div class="col-xs-4 col-md-2">
+    <div class="col-xs-12 col-sm-3 col-md-2">
       <?php print render($content['field_os2web_kulturnaut_slidesho']); ?>
     </div>
-    <div class="col-xs-8 col-md-10">
+    <div class="col-xs-12 col-sm-9 col-md-10">
       <div class="field-name-field-os2web-content-type">
         <?php print node_type_get_name($node); ?>
       </div>
