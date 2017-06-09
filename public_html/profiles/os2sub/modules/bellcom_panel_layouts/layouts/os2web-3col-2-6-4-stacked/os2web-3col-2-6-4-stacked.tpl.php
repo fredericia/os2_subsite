0766e5ce27537ec98687b02b6566846288625b9a
dsbellcom <ds@bellcom.ee>
2017-06-09 14:46:47 +0300

    Node details xs collapsable

--- public_html/profiles/os2sub/modules/bellcom_panel_layouts/layouts/os2web-3col-2-6-4-stacked/os2web-3col-2-6-4-stacked.tpl.php
+++ public_html/profiles/os2sub/modules/bellcom_panel_layouts/layouts/os2web-3col-2-6-4-stacked/os2web-3col-2-6-4-stacked.tpl.php
@@ -1,69 +1,82 @@
 <!-- content-with-left-and-right-sidebar.tpl.php -->
 
 
-<div class="row" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="row" <?php if (!empty($css_id)) {
+  print "id=\"$css_id\"";
+} ?>>
 
     <?php if ($content['top']): ?>
-	   	<!-- Begin - top -->
-		    <div class="col-md-12 content-top">
-		        <?php print $content['top']; ?>
-		    </div>
-		    <!-- End - top -->
-	<?php endif ?>
+    <!-- Begin - top -->
+    <div class="col-md-12 content-top">
+    <?php print $content['top']; ?>
+    </div>
+    <!-- End - top -->
+  <?php endif ?>
 
 
     <?php if ($content['sidebar-left']): ?>
-	    <!-- Begin - left sidebar -->
-	    <div class="col-md-2">
-	        <?php print $content['sidebar-left']; ?>
-	    </div>
-	    <!-- End - left sidebar -->
-	<?php endif ?>
+    <!-- Begin - left sidebar -->
+    <div class="col-md-2">
+    <?php print $content['sidebar-left']; ?>
+    </div>
+    <!-- End - left sidebar -->
+  <?php endif ?>
 
-    <?php if ($content['sidebar-right']): ?>
-    
-	    <!-- Begin - right sidebar -->
-	    <div class="col-md-4 col-md-push-6">
-	        <?php print $content['sidebar-right']; ?>
-	    </div>
-	    <!-- End - right sidebar -->
-	<?php endif ?>
+<?php if ($content['sidebar-right']): ?>
 
-	<?php if ($content['sidebar-left'] && $content['sidebar-right']): ?>
-    	
-    	<?php /* If both sidebars are present*/?>
-    	
-	    <!-- Begin - content -->
-	    <div class="col-md-6 col-md-pull-4">
-	        <?php print $content['content']; ?>
-	    </div>
-	    <!-- End - content -->
+    <!-- Begin - right sidebar -->
+    <div class="col-md-4 col-md-push-6">
+      <?php print $content['sidebar-right']; ?>
 
-	<?php elseif ($content['sidebar-left']): ?>
 
-    	<?php /* If left sidebar is present*/?>
+  <?php if ($content['sidebar-right-xs-collapsible']): ?>
 
-		<div class="col-md-10">
-	        <?php print $content['content']; ?>
-	    </div>
+        <!-- Begin - sidebar-right-xs-collapsible -->
+        <button data-toggle="collapse" data-target="#details" class="btn btn-tertiary btn-full-width hidden-md hidden-lg" ><?php print t('vis flere detaljer...'); ?></button>
+        <div id="details" class="collapse">
+        <?php print $content['sidebar-right-xs-collapsible']; ?>
+        </div>
+        <!-- End - right sidebar-right-xs-collapsible -->
+    <?php endif ?>
+    </div>
+    <!-- End - right sidebar -->
+  <?php endif ?>
 
-	<?php else : ?>
+  <?php if ($content['sidebar-left'] && $content['sidebar-right']): ?>
 
-    	<?php /* If no sidebars are present*/?>
-	
-		<div class="col-sm-12">
-	        <?php print $content['content']; ?>
-	    </div>
+  <?php /* If both sidebars are present */ ?>
 
-	<?php endif ?>
+    <!-- Begin - content -->
+    <div class="col-md-6 col-md-pull-4 vertical-line">
+  <?php print $content['content']; ?>
+    </div>
+    <!-- End - content -->
+
+  <?php elseif ($content['sidebar-left']): ?>
+
+      <?php /* If left sidebar is present */ ?>
+
+    <div class="col-md-10">
+    <?php print $content['content']; ?>
+    </div>
+
+  <?php else : ?>
+
+      <?php /* If no sidebars are present */ ?>
+
+    <div class="col-sm-12">
+    <?php print $content['content']; ?>
+    </div>
+
+  <?php endif ?>
 
     <?php if ($content['footer']): ?>
-	   	<!-- Begin - bottom -->
-		    <div class="col-md-12">
-		        <?php print $content['footer']; ?>
-		    </div>
-		    <!-- End - bottom -->
-	<?php endif ?>
+    <!-- Begin - bottom -->
+    <div class="col-md-12">
+    <?php print $content['footer']; ?>
+    </div>
+    <!-- End - bottom -->
+<?php endif ?>
 
 
 </div>
