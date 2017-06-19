9979b07f4492c9ab64ca7cea547e35d3074e5b30
Franz Skaaning <fs@bellcom.dk>
2017-06-19 15:12:16 +0200

    Top nbanner højde

--- public_html/profiles/os2sub/themes/custom/site_6juli/templates/node/teaser/node--banner--teaser.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_6juli/templates/node/teaser/node--banner--teaser.tpl.php
@@ -1,11 +1,10 @@
-
 <?php 
 	$style='os2sub_banner';
 	$path=$node->field_banner_billede['und']['0']['uri'];
 	$style_url = image_style_url($style, $path); ?>
 	
 	<article id="node-<?php print $node->nid; ?>" class="banner <?php print $classes . " all"; ?> clearfix"<?php print $attributes; ?> 
-		style="background: url(<?php print file_create_url($style_url); ?>) no-repeat scroll ;height:400px;background-size: cover;">
+		style="background: url(<?php print file_create_url($style_url); ?>) no-repeat scroll ;height:350px;background-size: cover;">
 	
 	<div class="row">
 		<div class="col-md-6 ">
@@ -17,13 +16,4 @@
 		</div>
 	</div>
 	</div>
-	
-
-  
-  
-  
-  
-  
 </article>
-
-
