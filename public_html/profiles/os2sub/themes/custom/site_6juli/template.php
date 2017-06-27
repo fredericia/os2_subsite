d5cfe0aacca1f1ea60a790be645c0caac6582627
Franz Skaaning <fs@bellcom.dk>
2017-06-27 16:17:35 +0200

    menu prøve igen

--- public_html/profiles/os2sub/themes/custom/site_6juli/template.php
+++ public_html/profiles/os2sub/themes/custom/site_6juli/template.php
@@ -115,3 +115,44 @@ function site_6juli_menu_local_tasks(&$variables) {
 
   return $output;
 }
+
+function site_6juli_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+  $element['#attributes']['class'][] = 'main_menu_li';
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    elseif ((!empty($element['#original_link']['depth']))) {
+      // Add our own wrapper.
+      unset($element['#below']['#theme_wrappers']);
+      $class =  "dropdown-menu";
+      $sub_menu = '<ul class="' . $class . '">' . drupal_render($element['#below']) . '</ul>';
+
+
+      // Generate as standard dropdown.
+      $element['#title'] .= ' <span class="caret"></span>';
+      $element['#attributes']['class'][] = 'dropdown';
+      $element['#localized_options']['html'] = TRUE;
+
+      // Set dropdown trigger element to # to prevent inadvertant page loading
+      // when a submenu link is clicked.
+      //$element['#localized_options']['attributes']['data-target'] = '#';
+      $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+      if (!$menu_location) {
+        $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+      }
+    }
+
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
