cd215d3b547daf91a7ea375bd633bf5be41d39ff
Franz Skaaning <fs@bellcom.dk>
2017-09-15 14:41:40 +0200

    if dims ændret

--- public_html/profiles/os2sub/themes/custom/site_alternative/templates/field/field--field-os2web-kulturnaut-url.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_alternative/templates/field/field--field-os2web-kulturnaut-url.tpl.php
@@ -44,15 +44,16 @@
  * @ingroup themeable
  */
 ?>
-<?php if (!empty($items)) : ?>
+
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!$label_hidden): ?>
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <a class="btn btn-full-width" href="<?php print render($item); ?>"><?php print t('Køb billet');?></a>
+      <?php if (!empty($item)) : ?>
+        <a class="btn btn-full-width" href="<?php print render($item); ?>"><?php print t('Køb billet');?></a>
+      <?php endif; ?>
     <?php endforeach; ?>
   </div>
 </div>
-<?php endif; ?>
