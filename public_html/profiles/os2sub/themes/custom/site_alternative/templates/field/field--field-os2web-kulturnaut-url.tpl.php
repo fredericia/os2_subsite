fe3b510f99be38218f8f2e11f233d86a31fda09e
Dmitri Serõn <ds@bellcom.ee>
2017-09-19 10:00:13 +0300

    oplev FKDK-786 no billet link if billet url is blank

--- public_html/profiles/os2sub/themes/custom/site_alternative/templates/field/field--field-os2web-kulturnaut-url.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_alternative/templates/field/field--field-os2web-kulturnaut-url.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file field.tpl.php
  * Default template implementation to display the value of a field.
@@ -44,13 +43,16 @@
  * @ingroup themeable
  */
 ?>
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
-  <?php endif; ?>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <a class="btn btn-full-width" href="<?php print render($item); ?>"><?php print t('Køb billet');?></a>
-    <?php endforeach; ?>
+
+<?php if ( isset($items) ): ?>
+  <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php if ( !$label_hidden ): ?>
+      <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+    <?php endif; ?>
+    <div class="field-items"<?php print $content_attributes; ?>>
+      <?php foreach ($items as $delta => $item): ?>
+        <a class="btn btn-full-width" href="<?php print render($item); ?>"><?php print t('Køb billet'); ?></a>
+      <?php endforeach; ?>
+    </div>
   </div>
-</div>
+<?php endif; ?>
