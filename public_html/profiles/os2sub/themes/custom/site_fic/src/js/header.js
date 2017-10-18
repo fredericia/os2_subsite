ba9de9f0dacb07ac61a70887ca4c3451467fd939
Andriy Iun <andriy.yun@gmail.com>
2017-10-18 20:09:56 +0200

    FIC Modal popup

--- public_html/profiles/os2sub/themes/custom/site_fic/src/js/header.js
+++ public_html/profiles/os2sub/themes/custom/site_fic/src/js/header.js
@@ -3,54 +3,65 @@
 //// |--------------------------------------------------------------------------
 
 var header = (function ($) {
-   'use strict';
+  'use strict';
 
-   var pub = {};
+  var pub = {};
 
-   /**
-    * Instantiate
-    */
-   pub.init = function () {};
+  /**
+  * Instantiate
+  */
+  pub.init = function () {};
 
-   /**
-    * Custom backstretch behavior.
-    */
-   Drupal.behaviors.ficBackstretch = {
-     attach: function (context, settings) {
-       if (typeof settings.ficBackstretch === 'undefined') {
-         return;
-       }
-
-       var $images = [],
-         $backstretchWrapper = $('.page-header-wrapper');
-       for (var i = 0; i < settings.ficBackstretch.length; i++) {
-         $images.push(settings.ficBackstretch[i].url);
-       }
-       $backstretchWrapper.backstretch($images, {
-         // Slides should be never changed automaticly.
-         duration : 1000000
-       });
-     },
-
-     show : function($termId) {
-       var $i = 0,
-         $backstretch = $('.page-header-wrapper').data('backstretch');
-
-       if (typeof Drupal.settings.ficBackstretch === 'undefined'
-        || typeof $backstretch === 'undefined') {
-         return;
-       }
-
-       for (var i = 0; i < Drupal.settings.ficBackstretch.length; i++) {
-           if ($termId == Drupal.settings.ficBackstretch[i].tid) {
-           $backstretch.show($i);
-           break;
-         }
-         $i++;
-       }
+  /**
+  * Custom backstretch behavior.
+  */
+  Drupal.behaviors.ficBackstretch = {
+   attach: function (context, settings) {
+     if (typeof settings.ficBackstretch === 'undefined') {
+       return;
      }
-   };
 
-   return pub;
+     var $images = [],
+       $backstretchWrapper = $('.page-header-wrapper');
+     for (var i = 0; i < settings.ficBackstretch.length; i++) {
+       $images.push(settings.ficBackstretch[i].url);
+     }
+     $backstretchWrapper.backstretch($images, {
+       // Slides should be never changed automaticly.
+       duration : 1000000
+     });
+   },
+
+   show : function($termId) {
+     var $i = 0,
+       $backstretch = $('.page-header-wrapper').data('backstretch');
+
+     if (typeof Drupal.settings.ficBackstretch === 'undefined'
+      || typeof $backstretch === 'undefined') {
+       return;
+     }
+
+     for (var i = 0; i < Drupal.settings.ficBackstretch.length; i++) {
+         if ($termId == Drupal.settings.ficBackstretch[i].tid) {
+         $backstretch.show($i);
+         break;
+       }
+       $i++;
+     }
+   }
+  };
+
+  Drupal.theme.prototype.fic_modal = function () {
+    var html = '';
+    html += '<div id="ctools-modal" class="popups-box my-first-popup">';
+    html += ' <div class="ctools-modal-content my-popup ">';
+    html += ' <span class="popups-close"><a class="close" href="#">×</a></span>';
+    html += ' <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+    html += ' </div>';
+    html += '</div>';
+    return html;
+  };
+
+  return pub;
 
 })(jQuery);
