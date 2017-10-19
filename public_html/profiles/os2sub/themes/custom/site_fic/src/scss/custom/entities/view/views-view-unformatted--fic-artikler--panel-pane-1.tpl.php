9e5b24b3498b16b9ccd7ccda2ab0b78d016bc4b5
Dmitri Serõn <ds@bellcom.ee>
2017-10-19 16:30:45 +0300

    FIC fic_artikler styles

---
+++ public_html/profiles/os2sub/themes/custom/site_fic/src/scss/custom/entities/view/views-view-unformatted--fic-artikler--panel-pane-1.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if ( !empty($title) ): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div<?php
+  if ( $classes_array[$id] ) {
+    print ' class="' . $classes_array[$id] . '"';
+  }
+  ?>>
+      <?php print $row; ?>
+  </div>
+<?php endforeach; ?>
+<div class="views-row views-row views-row-se-flere">
+
+  <a href='#'>
+    <article class="se-flere">
+      <div class="field field-name-field-os2web-base-field-lead-img field-type-image field-label-hidden">
+        <img class="img-responsive" src="/profiles/os2sub/themes/custom/site_fic/dist/img/1px_transparent.png" width="670" height="670">
+      </div>
+      <div class='se-flere-content'>
+        <span>
+          <p class='se-flere-icon'>+</p>
+          <p><?php print t('se flere'); ?></p>
+        </span>
+      </div>
+    </article>
+  </a>
+</div>
