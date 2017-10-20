bfa2ea2e3c4a2e01a04fa9a68a11a3d38e78c8e4
franzskaaning <fs@bellcom.dk>
2017-10-20 23:24:08 +0200

    Border top

--- public_html/profiles/os2sub/themes/custom/site_fic/templates/panel-pane/panels-pane--fw-views.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_fic/templates/panel-pane/panels-pane--fw-views.tpl.php
@@ -6,36 +6,38 @@
   <?php endif; ?>
   <div class="views-container full-width <?php print $classes; ?>" <?php print $id; ?> <?php print $attributes; ?>>
     <div class="views-container-inner">
-      <?php if ($admin_links): ?>
-        <?php print $admin_links; ?>
-      <?php endif; ?>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <<?php print $title_heading; ?><?php print $title_attributes; ?>>
-          <?php print $title; ?>
-        </<?php print $title_heading; ?>>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-  
-      <?php if ($feeds): ?>
-        <div class="feed">
-          <?php print $feeds; ?>
+      <div class="views-container-inner-inner">
+        <?php if ($admin_links): ?>
+          <?php print $admin_links; ?>
+        <?php endif; ?>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <<?php print $title_heading; ?><?php print $title_attributes; ?>>
+            <?php print $title; ?>
+          </<?php print $title_heading; ?>>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+    
+        <?php if ($feeds): ?>
+          <div class="feed">
+            <?php print $feeds; ?>
+          </div>
+        <?php endif; ?>
+        <div class="pane-content">
+            <?php print render($content); ?>
         </div>
-      <?php endif; ?>
-      <div class="pane-content">
-          <?php print render($content); ?>
+        <?php if ($links): ?>
+          <div class="links">
+            <?php print $links; ?>
+          </div>
+        <?php endif; ?>
+    
+        <?php if ($more): ?>
+          <div class="more-link">
+            <?php print $more; ?>
+          </div>
+        <?php endif; ?>        
       </div>
-      <?php if ($links): ?>
-        <div class="links">
-          <?php print $links; ?>
-        </div>
-      <?php endif; ?>
-  
-      <?php if ($more): ?>
-        <div class="more-link">
-          <?php print $more; ?>
-        </div>
-      <?php endif; ?>
     </div>
   </div>
   <?php if ($pane_suffix): ?>
