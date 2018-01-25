960df08a278ee6d0a04dfd6bb5dbe29332b4f7ae
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-25 10:57:36 +0100

    FKDK-879 - boxes
    Restructure JS file so it’s more readable

--- public_html/profiles/os2sub/themes/custom/site_fic/src/js/header.js
+++ public_html/profiles/os2sub/themes/custom/site_fic/src/js/header.js
@@ -3,166 +3,171 @@
 //// |--------------------------------------------------------------------------
 
 var header = (function ($) {
-  'use strict';
+    'use strict';
 
-  var pub = {};
+    var pub = {};
 
-  /**
-  * Instantiate
-  */
-  pub.init = function () {
+    /**
+     * Instantiate
+     */
+    pub.init = function () {
         registerBootEventHandlers();
         registerEventHandlers();
-  };
+    };
 
-  /**
-   * Register boot event handlers.
-   */
-  function registerBootEventHandlers() {}
-
-  /**
-   * Register event handlers.
-   */
-  function registerEventHandlers() {
-    // Add submenu open/close behavior.
-    $('.headerwrapper-inner .navbar-nav > li.expanded').hover(
-      function() { $(this).addClass('open').children('a').attr('aria-expanded', 'true');},
-      function() { $(this).removeClass('open').children('a').attr('aria-expanded', 'false');}
-    ).click(function(event) {
-      // Disable other event listeners.
-      event.stopPropagation();
-      // Restore default link behaviour.
-    });
-
-    //Smooth scroll to #main-content
-    $("#scroll-down-link").on('click', function (e) {
-      e.preventDefault();
-      $('html, body').animate({
-        scrollTop: $("#main-content").offset().top
-      }, 500);
-    });
-
-  }
-
-  /**
-  * Custom backstretch behavior.
-  */
-  Drupal.behaviors.ficBackstretch = {
-   attach: function (context, settings) {
-     if (typeof settings.ficBackstretch === 'undefined'
-       || $('.backstretch').length > 0) {
-       return;
-     }
-
-     var $images = [],
-       $backstretchWrapper = $('.page-header-wrapper');
-     for (var i = 0; i < settings.ficBackstretch.length; i++) {
-       $images.push(settings.ficBackstretch[i].url);
-     }
-     $backstretchWrapper.backstretch($images, {
-       // Slides should be never changed automaticly.
-       duration : 1000000, fade: 750, paused: true
-     });
-   },
-
-   show : function($id) {
-     var $i = 0,
-       $backstretch = $('.page-header-wrapper').data('backstretch'),
-       $default = true;
-
-     if (typeof Drupal.settings.ficBackstretch === 'undefined'
-      || typeof $backstretch === 'undefined') {
-       return;
-     }
-
-
-     for (var i = 0; i < Drupal.settings.ficBackstretch.length; i++) {
-         if ($id == Drupal.settings.ficBackstretch[i].id) {
-         $backstretch.show($i);
-         $default = false;
-         break;
-       }
-       $i++;
-     }
-
-     if ($default) {
-       $backstretch.show(0);
-     }
-   }
-  };
-
-  /**
-   * Custom headerRegion behavior.
-   */
-  Drupal.behaviors.headerRegion = {
-    attach: function (context, settings) {
-      if (!$('.term-fic-header').length) {
-        return;
-      }
-
-      var $wH = $(window).height(),
-        // All static value defiden accordingly with design.
-        $brandigH = 178 + 20,
-        $navigationH = 150 + 20,
-        $scrollDownH = 174,
-        $descrH = $wH - $brandigH - $navigationH - $scrollDownH,
-        $minH = 300,
-        $maxH = 700;
-
-      $descrH = $descrH < $minH ? $minH : ($descrH > $maxH ? $maxH : $descrH);
-      $('.term-fic-header .views_slideshow_main, .page-taxonomy .term-fic-header > .taxonomy-term .cycle-slideshow').height($descrH);
+    /**
+     * Register boot event handlers.
+     */
+    function registerBootEventHandlers() {
     }
-  };
 
-  Drupal.theme.prototype.fic_modal = function () {
-    var html = '';
-    html += '<div id="ctools-modal" class="popups-box my-first-popup">';
-    html += ' <div class="ctools-modal-content my-popup ">';
-    html += ' <span class="popups-close"><a class="close" href="#">×</a></span>';
-    html += ' <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
-    html += ' </div>';
-    html += '</div>';
-    return html;
-  };
+    /**
+     * Register event handlers.
+     */
+    function registerEventHandlers() {
+        // Add submenu open/close behavior.
+        $('.headerwrapper-inner .navbar-nav > li.expanded').hover(
+            function () {
+                $(this).addClass('open').children('a').attr('aria-expanded', 'true');
+            },
+            function () {
+                $(this).removeClass('open').children('a').attr('aria-expanded', 'false');
+            }
+        ).click(function (event) {
+            // Disable other event listeners.
+            event.stopPropagation();
+            // Restore default link behaviour.
+        });
 
-  /**
-   * Custom ficHeaderCycleSlideshow behavior.
-   */
-  Drupal.behaviors.ficHeaderCycleSlideshow = {
-    attach: function (context, settings) {
-      var $slideShow = $('.cycle-slideshow');
-      if (!$slideShow.once().length) {
-        return;
-      }
+        //Smooth scroll to #main-content
+        $("#scroll-down-link").on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $("#main-content").offset().top
+            }, 500);
+        });
 
-      $slideShow.cycle({
-        speed: 700,
-        timeout: 0,
-        pager: '#cycle-nav',
-        before: function(currSlideElement, nextSlideElement, options, forwardFlag) {
-          var $termId = $(nextSlideElement).data('id');
-          if (typeof Drupal.behaviors.ficBackstretch !== "undefined") {
-            Drupal.behaviors.ficBackstretch.show($termId);
-          }
-        },
-        pagerAnchorBuilder: function(idx, slide) {
-          var $slide = $(slide),
-            $target = '';
-          if (typeof $slide.data('target') !== 'undefined') {
-            $target = ' target="' + $slide.data('target') + '"';
-          }
-          return '<div class="field-item"><a href="' + $slide.data('url') + '"' + $target + '><span>' + $slide.data('name') + '</span></a></div>';
-        },
-        pagerEvent: 'mouseover',
-        pauseOnPagerHover: true
-      });
-
-      $('.cycle-pager a').on('click', function(e){
-        e.stopPropagation();
-      });
     }
-  };
 
-  return pub;
+    /**
+     * Custom backstretch behavior.
+     */
+    Drupal.behaviors.ficBackstretch = {
+        attach: function (context, settings) {
+            if (typeof settings.ficBackstretch === 'undefined'
+                || $('.backstretch').length > 0) {
+                return;
+            }
+
+            var $images = [],
+                $backstretchWrapper = $('.page-header-wrapper');
+            for (var i = 0; i < settings.ficBackstretch.length; i++) {
+                $images.push(settings.ficBackstretch[i].url);
+            }
+            $backstretchWrapper.backstretch($images, {
+                // Slides should be never changed automaticly.
+                duration: 1000000, fade: 750, paused: true
+            });
+        },
+
+        show: function ($id) {
+            var $i = 0,
+                $backstretch = $('.page-header-wrapper').data('backstretch'),
+                $default = true;
+
+            if (typeof Drupal.settings.ficBackstretch === 'undefined'
+                || typeof $backstretch === 'undefined') {
+                return;
+            }
+
+
+            for (var i = 0; i < Drupal.settings.ficBackstretch.length; i++) {
+                if ($id == Drupal.settings.ficBackstretch[i].id) {
+                    $backstretch.show($i);
+                    $default = false;
+                    break;
+                }
+                $i++;
+            }
+
+            if ($default) {
+                $backstretch.show(0);
+            }
+        }
+    };
+
+    /**
+     * Custom headerRegion behavior.
+     */
+    Drupal.behaviors.headerRegion = {
+        attach: function (context, settings) {
+            if (!$('.term-fic-header').length) {
+                return;
+            }
+
+            var $wH = $(window).height(),
+                // All static value defiden accordingly with design.
+                $brandigH = 178 + 20,
+                $navigationH = 150 + 20,
+                $scrollDownH = 174,
+                $descrH = $wH - $brandigH - $navigationH - $scrollDownH,
+                $minH = 300,
+                $maxH = 700;
+
+            $descrH = $descrH < $minH ? $minH : ($descrH > $maxH ? $maxH : $descrH);
+            $('.term-fic-header .views_slideshow_main, .page-taxonomy .term-fic-header > .taxonomy-term .cycle-slideshow').height($descrH);
+        }
+    };
+
+    Drupal.theme.prototype.fic_modal = function () {
+        var html = '';
+        html += '<div id="ctools-modal" class="popups-box my-first-popup">';
+        html += ' <div class="ctools-modal-content my-popup ">';
+        html += ' <span class="popups-close"><a class="close" href="#">×</a></span>';
+        html += ' <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+        html += ' </div>';
+        html += '</div>';
+        return html;
+    };
+
+    /**
+     * Custom ficHeaderCycleSlideshow behavior.
+     */
+    Drupal.behaviors.ficHeaderCycleSlideshow = {
+        attach: function (context, settings) {
+            var $slideShow = $('.cycle-slideshow');
+            if (!$slideShow.once().length) {
+                return;
+            }
+
+            $slideShow.cycle({
+                speed: 700,
+                timeout: 0,
+                pager: '#cycle-nav',
+                before: function (currSlideElement, nextSlideElement, options, forwardFlag) {
+                    var $termId = $(nextSlideElement).data('id');
+                    if (typeof Drupal.behaviors.ficBackstretch !== "undefined") {
+                        Drupal.behaviors.ficBackstretch.show($termId);
+                    }
+                },
+                pagerAnchorBuilder: function (idx, slide) {
+                    var $slide = $(slide),
+                        $target = '';
+                    if (typeof $slide.data('target') !== 'undefined') {
+                        $target = ' target="' + $slide.data('target') + '"';
+                    }
+                    return '<div class="field-item"><a href="' + $slide.data('url') + '"' + $target + '><span>' + $slide.data('name') + '</span></a></div>';
+                },
+                pagerEvent: 'mouseover',
+                pauseOnPagerHover: true
+            });
+
+            $('.cycle-pager a').on('click', function (e) {
+                e.stopPropagation();
+            });
+        }
+    };
+
+    return pub;
 
 })(jQuery);
