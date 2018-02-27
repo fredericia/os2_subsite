3d83fb0da002c69442913ff1ccd1c59c66870e06
Morten Nissen <mail@hjemmesidekongen.dk>
2018-02-27 10:51:13 +0100

    FKDK-1059

--- public_html/profiles/os2sub/themes/custom/site_fic/templates/paragraph/paragraphs-item--fic-list-of-event-teasers.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_fic/templates/paragraph/paragraphs-item--fic-list-of-event-teasers.tpl.php
@@ -1,7 +1,15 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="content"<?php print $content_attributes; ?>>
-    <?php print render($content); ?>
+
+    <?php if ( isset( $content['field_paragraph_header'] ) ): ?>
+        <!-- Begin - heading -->
+        <div class="paragraph__heading">
+          <h2 class="paragraph__heading__title"><?php print render( $content['field_paragraph_header'] ); ?></h2>
+        </div>
+        <!-- End - heading -->
+    <?php endif; ?>
 
     <?php print $embedded_view; ?>
   </div>
 </div>
+
