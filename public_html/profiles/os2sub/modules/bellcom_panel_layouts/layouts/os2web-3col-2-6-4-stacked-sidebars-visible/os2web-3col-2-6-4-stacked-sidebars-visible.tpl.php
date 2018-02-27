c479d5b2e9f33be9ec0ac724144fc4dc7fdd7245
Stanislav Kutasevits <stan@bellcom.dk>
2018-02-27 16:57:21 +0200

    6juli, FKDK-1073 introducing panel where sidebars are always present

---
+++ public_html/profiles/os2sub/modules/bellcom_panel_layouts/layouts/os2web-3col-2-6-4-stacked-sidebars-visible/os2web-3col-2-6-4-stacked-sidebars-visible.tpl.php
@@ -0,0 +1,50 @@
+<!-- content-with-left-and-right-sidebar.tpl.php -->
+
+
+<div class="row" <?php if (!empty($css_id)) {
+  print "id=\"$css_id\"";
+} ?>>
+
+  <?php if ($content['top']): ?>
+    <!-- Begin - top -->
+    <div class="col-md-12 content-top">
+      <?php print $content['top']; ?>
+    </div>
+    <!-- End - top -->
+  <?php endif ?>
+
+
+  <!-- Begin - left sidebar -->
+  <div class="col-md-2">
+    <?php if ($content['sidebar-left']): ?>
+      <?php print $content['sidebar-left']; ?>
+    <?php endif ?>
+  </div>
+  <!-- End - left sidebar -->
+
+  <!-- Begin - content -->
+  <div class="col-md-6 vertical-line">
+    <?php if ($content['content']): ?>
+      <?php print $content['content']; ?>
+    <?php endif ?>
+  </div>
+  <!-- End - content -->
+
+  <!-- Begin - right sidebar -->
+  <div class="col-md-4">
+    <?php if ($content['sidebar-right']): ?>
+      <?php print $content['sidebar-right']; ?>
+    <?php endif ?>
+  </div>
+  <!-- End - right sidebar -->
+
+  <?php if ($content['footer']): ?>
+    <!-- Begin - bottom -->
+    <div class="col-md-12">
+      <?php print $content['footer']; ?>
+    </div>
+    <!-- End - bottom -->
+  <?php endif ?>
+
+
+</div>
