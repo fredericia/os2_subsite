83f3549a50d83a570dc465bfd36e507a8a844424
Morten Nissen <mail@hjemmesidekongen.dk>
2018-03-01 13:52:52 +0100

    FKDK-1073

--- public_html/profiles/os2sub/modules/bellcom_panel_layouts/layouts/os2web-3col-2-6-4-stacked-sidebars-visible/os2web-3col-2-6-4-stacked-sidebars-visible.tpl.php
+++ public_html/profiles/os2sub/modules/bellcom_panel_layouts/layouts/os2web-3col-2-6-4-stacked-sidebars-visible/os2web-3col-2-6-4-stacked-sidebars-visible.tpl.php
@@ -7,44 +7,50 @@
 
   <?php if ($content['top']): ?>
     <!-- Begin - top -->
-    <div class="col-md-12 content-top">
-      <?php print $content['top']; ?>
+    <div class="row">
+      <div class="col-md-12 content-top">
+        <?php print $content['top']; ?>
+      </div>
     </div>
     <!-- End - top -->
   <?php endif ?>
 
+  <div class="row">
 
-  <!-- Begin - left sidebar -->
-  <div class="col-md-2">
-    <?php if ($content['sidebar-left']): ?>
-      <?php print $content['sidebar-left']; ?>
-    <?php endif ?>
-  </div>
-  <!-- End - left sidebar -->
+    <!-- Begin - left sidebar -->
+    <div class="col-md-2">
+      <?php if ($content['sidebar-left']): ?>
+        <?php print $content['sidebar-left']; ?>
+      <?php endif ?>
+    </div>
+    <!-- End - left sidebar -->
 
-  <!-- Begin - content -->
-  <div class="col-md-6 vertical-line">
-    <?php if ($content['content']): ?>
-      <?php print $content['content']; ?>
-    <?php endif ?>
-  </div>
-  <!-- End - content -->
+    <!-- Begin - content -->
+    <div class="col-md-6 vertical-line">
+      <?php if ($content['content']): ?>
+        <?php print $content['content']; ?>
+      <?php endif ?>
+    </div>
+    <!-- End - content -->
 
-  <!-- Begin - right sidebar -->
-  <div class="col-md-4">
-    <?php if ($content['sidebar-right']): ?>
-      <?php print $content['sidebar-right']; ?>
-    <?php endif ?>
+    <!-- Begin - right sidebar -->
+    <div class="col-md-4">
+      <?php if ($content['sidebar-right']): ?>
+        <?php print $content['sidebar-right']; ?>
+      <?php endif ?>
+    </div>
+    <!-- End - right sidebar -->
+
   </div>
-  <!-- End - right sidebar -->
 
   <?php if ($content['footer']): ?>
     <!-- Begin - bottom -->
-    <div class="col-md-12">
-      <?php print $content['footer']; ?>
+    <div class="row">
+      <div class="col-md-12">
+        <?php print $content['footer']; ?>
+      </div>
     </div>
     <!-- End - bottom -->
   <?php endif ?>
-
 
 </div>
