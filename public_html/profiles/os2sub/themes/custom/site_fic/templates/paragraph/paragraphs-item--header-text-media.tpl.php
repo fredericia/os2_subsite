1978596855b3917ea8da8f5c528619b751f00a9f
Morten Nissen <mail@hjemmesidekongen.dk>
2018-03-08 13:55:31 +0100

    FKDK-1031

--- public_html/profiles/os2sub/themes/custom/site_fic/templates/paragraph/paragraphs-item--header-text-media.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_fic/templates/paragraph/paragraphs-item--header-text-media.tpl.php
@@ -1,37 +1,13 @@
-<?php
-/**
- * @file
- * Default theme implementation for a single paragraph item.
- *
- * Available variables:
- * - $content: An array of content items. Use render($content) to print them
- *   all, or print a subset such as render($content['field_example']). Use
- *   hide($content['field_example']) to temporarily suppress the printing of a
- *   given element.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
- *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity
- *   - entity-paragraphs-item
- *   - paragraphs-item-{bundle}
- *
- * Other variables:
- * - $classes_array: Array of html class attribute values. It is flattened into
- *   a string within the variable $classes.
- *
- * @see template_preprocess()
- * @see template_preprocess_entity()
- * @see template_process()
- */
-?>
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
     <div class="container">
+
       <?php if ($position_of_media == 'left'): ?>
-        <div class="row">
-          <div class="col-xs-12 col-lg-6 paragraph-video-container">
-            <div class="paragraph-video">
+
+        <div class="position-of-media position-of-media--left">
+          <div class="row">
+            <div class="col-xs-12 col-lg-6">
+
               <?php if ($show_media_in_modal): ?>
                 <a href="<?=$url_for_media_modal; ?>" data-lity>
                   <?php print render($content['field_image']); ?>
@@ -39,25 +15,25 @@
               <?php else: ?>
                 <?php print render($content['field_image']); ?>
               <?php endif; ?>
+
             </div>
-          </div>
-          <div class="col-xs-12 col-lg-6">
-            <div class="aligner">
+            <div class="col-xs-12 col-lg-6">
               <?php print render($content['field_paragraph_header']); ?>
               <?php print render($content['field_paragraph_text']); ?>
             </div>
           </div>
         </div>
+
       <?php elseif ($position_of_media == 'right'): ?>
-        <div class="row">
-          <div class="col-xs-12 col-lg-6">
-            <div class="aligner">
+
+        <div class="position-of-media position-of-media--right">
+          <div class="row">
+            <div class="col-xs-12 col-lg-6">
               <?php print render($content['field_paragraph_header']); ?>
               <?php print render($content['field_paragraph_text']); ?>
             </div>
-          </div>
-          <div class="col-xs-12 col-lg-6 paragraph-video-container">
-            <div class="paragraph-video">
+            <div class="col-xs-12 col-lg-6">
+
               <?php if ($show_media_in_modal): ?>
                 <a href="<?=$url_for_media_modal; ?>" data-lity>
                   <?php print render($content['field_image']); ?>
@@ -65,30 +41,35 @@
               <?php else: ?>
                 <?php print render($content['field_image']); ?>
               <?php endif; ?>
+
             </div>
           </div>
         </div>
+
       <?php else : ?>
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="paragraph-video">
-            <?php if ($show_media_in_modal): ?>
-              <a href="<?=$url_for_media_modal; ?>" data-lity>
+        <div class="position-of-media position-of-media--centered">
+          <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-push-2">
+
+              <?php if ($show_media_in_modal): ?>
+                <a href="<?=$url_for_media_modal; ?>" data-lity>
+                  <?php print render($content['field_image']); ?>
+                </a>
+              <?php else: ?>
                 <?php print render($content['field_image']); ?>
-              </a>
-            <?php else: ?>
-              <?php print render($content['field_image']); ?>
-            <?php endif; ?>
+              <?php endif; ?>
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-push-2">
+              <?php print render($content['field_paragraph_header']); ?>
+              <?php print render($content['field_paragraph_text']); ?>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <?php print render($content['field_paragraph_header']); ?>
-          <?php print render($content['field_paragraph_text']); ?>
-        </div>
-      </div>
       <?php endif; ?>
+
     </div>
   </div>
 </div>
