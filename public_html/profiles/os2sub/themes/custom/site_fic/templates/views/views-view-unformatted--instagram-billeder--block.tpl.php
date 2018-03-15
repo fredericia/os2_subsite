66d6e1988c93d19ce2385aa5a180c8ef17a78545
Morten Nissen <mail@hjemmesidekongen.dk>
2018-03-15 08:36:59 +0100

    FKDK-1039

--- public_html/profiles/os2sub/themes/custom/site_fic/templates/views/views-view-unformatted--instagram-billeder--block.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_fic/templates/views/views-view-unformatted--instagram-billeder--block.tpl.php
@@ -19,6 +19,6 @@
 
 <?php foreach ($rows as $id => $row): ?>
   <div<?php if ($classes_array[$id]) {print ' class="' . $classes_array[$id] . '"';} ?>>
-  <?php print $row; ?>
+    <?php print $row; ?>
   </div>
 <?php endforeach; ?>
