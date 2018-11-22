0e8608d5061ec98d7da2a98ac130f6e5a0881ead
Morten Nissen <mail@hjemmesidekongen.dk>
2018-11-22 08:59:27 +0100

    #DVT-566-92571

--- public_html/profiles/os2sub/themes/custom/site_fic/templates/taxonomy-term/header/taxonomy-term--os2web-base-tax-site-structure--fic-header.tpl.php
+++ public_html/profiles/os2sub/themes/custom/site_fic/templates/taxonomy-term/header/taxonomy-term--os2web-base-tax-site-structure--fic-header.tpl.php
@@ -22,8 +22,7 @@
               <div class="blue-block">
                     <a href="<?php print $opening_hours_node_url; ?>"
                        class="ctools-modal-contact-modal-style <?php if ($opening_hours_open_type == 'modal') { print "ctools-use-modal"; } ?>">
-                        <span class="text hidden-xs"><?php print $opening_hours_main_text; ?></span>
-                        <span class="text visible-xs"><?php print t('Opening hours'); ?></span>
+                        <span class="text"><?php print $opening_hours_main_text; ?></span>
                         <?php if (!empty($opening_hours_sub_text)) : ?>
                           <span class="read-more hidden-xs"><?php print $opening_hours_sub_text; ?></span>
                         <?php endif; ?>
